a9824097a5b6791d9950dca494ecad61f3e785b4
Bckr <clemens.becker@outlook.com>
2018-01-25 23:40:56 +0100

    .

--- Code/comprehendverarbeitung/compproc.js
+++
@@ -1,9 +0,0 @@
-//Importieren der AWS-SDK um mit dieser arbeiten zu können
-var AWS = require('aws-sdk');
-
-var sns = new AWS.SNS();
-
-exports.handler = function (event,context,callback) {
-    console.log(event);
-    callback(null, "Finished");
-};
