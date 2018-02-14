7e86b0cfa91490a1d4bc0db999bd9a7ddeedc6c8
Bckr <clemens.becker@outlook.com>
2018-02-14 11:18:28 +0100

    Comprehendfunktionalität angefangen hinzuzufügen

---
+++ comprehendverarbeitung/comprehendLambda.js
@@ -0,0 +1,48 @@
+//Importieren der AWS-SDK um mit dieser arbeiten zu können
+var AWS = require('aws-sdk');
+
+//Erstellen einer SNS Instanz um die Daten per Mail verschicken zu können
+//Erstellen einer Comprehend Instanz, welche die Analyse der Rohdaten übernimmt
+//var sns = new AWS.SNS();
+
+var comprehend = new AWS.Comprehend();
+var a = [];
+
+//Handler, welcher als Einstiegspunkt für Lambda dient
+exports.handler = function (event,context,callback) {
+
+    //zieht aus dem Event, welches von der S3 Funktion ausgelöst wurde, die relevanten Daten
+    var incdata = event.Records[0].Sns.Message;
+
+    //Parameter, die Sprache und Inputtext, für Comprehend spezifizieren
+    var params = {
+        LanguageCode: 'en',
+        Text: incdata
+    };
+
+    //sucht im übergebenen Text nach Keyphrases und gibt diese zurück
+    comprehend.detectKeyPhrases(params,function (err, data) {
+        if(err) console.log(err);
+        else
+            var workdata = data.KeyPhrases;
+        for(var i = 0; i < workdata.length; ++i){
+            a.push(workdata[i].Text);
+        }
+        console.log(a);
+    });
+
+};
+
+
+/*
+//gibt die Daten per Email an die angelegte Mailadresse von Storm aus
+    var params = {
+        Message: incdata,
+        TopicArn: 'arn:aws:sns:us-east-1:647707457335:ComprehendTest',
+        Subject: 'Daten angekommen'
+    };
+
+    sns.publish(params,function (err, data3) {
+        if(err) console.log(err);
+        else console.log("Publication finished");
+    })*/
--- s3verarbeitung/s3Lambda.js
+++ s3verarbeitung/s3Lambda.js
@@ -2,9 +2,10 @@
 var AWS = require('aws-sdk');
 //Erstellen einer S3 Instanz und locken der API-Version
 var s3 = new AWS.S3({region: 'us-east-1', apiVersion: '2006-03-01'});
+//Erstellen einer SNS Instanz, um
 var sns = new AWS.SNS();
 
-//Herausfiltern des Bucket names und des Dateinamens aus den übergebenen Eventdaten
+//Herausfiltern des Bucketnamen und Objektkeys aus den Eventdaten
 exports.handler = function(event,context,callback){
     var bucket = event.Records[0].s3.bucket.name;
     var key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
@@ -12,32 +13,43 @@ exports.handler = function(event,context,callback){
         Bucket : bucket,
         Key: key,
     };
+
+    //Lieferung der Daten des Objektes und senden dieser Daten an eine SNS Topic
     s3.getObject(params, function (err, data) {
         if (!err) {
-            console.log("Es kamen Daten an und wir sind in die richtige Anweisung gesprungen");
             var incdata = data.Body;
             var convdata = incdata.toString();
 
             var params = {
                 Message: convdata,
-                TopicArn: 'arn:aws:sns:us-east-1:647707457335:Emailverarbeitung',
-                Subject: 'lambdatest'
+                TopicArn: 'arn:aws:sns:us-east-1:647707457335:Emailverarbeitung'
             };
 
             sns.publish(params, function (err2, data2){
                 if(err2) console.log(err2);
                 else console.log("Topic Publication finished with" + data2);
                 });
-            callback(null, "Job finished");
         }
         else {
             console.log(err);
-            const message = `Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`;
+            const message = "Error getting object" + ${key} + "from bucket" + ${bucket} + ". Make sure they exist and your bucket is in the same region as this function.";
             console.log(message);
             callback(message);
         }
     })
 };
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
