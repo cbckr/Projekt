ca20a5b01e45bf1d8ccdab53a5b8c13e1f64d383
Bckr <clemens.becker@outlook.com>
2018-02-21 00:27:38 +0100

    Funktionalität der Autoverarbeitung in test.js integrieret

--- s3verarbeitung/test.js
+++ s3verarbeitung/test.js
@@ -1,11 +1,52 @@
-var fs = require('fs');
+// var fs = require('fs');
 var simpleparser = require('mailparser').simpleParser;
-var daten = fs.readFileSync('./mimemail.txt').toString();
+// var daten = fs.readFileSync('./mimemail.txt').toString();
+
+// simpleparser(daten,function(err,data){
+//     if(err) console.log(err);
+//     else var tst = [];
+//             tst.push(data.from.text,data.to.text,data.subject,data.text);
+//             for(var i = 0; i < tst.length; ++i){
+//                 console.log(tst[i]);
+//             }
+// });
+
+// import entire SDK
+var AWS = require('aws-sdk');
+//Erstellen einer S3 Instanz und locken der API-Version
+var s3 = new AWS.S3({region: 'us-east-1', apiVersion: '2006-03-01'});
+//Erstellen einer SNS Instanz, um
+var sns = new AWS.SNS();
+
+//Herausfiltern des Bucketnamen und Objektkeys aus den Eventdaten
+exports.handler = function(event,context,callback){
+    var bucket = event.Records[0].s3.bucket.name;
+    var key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+    var params = {
+        Bucket : bucket,
+        Key: key
+    };
+
+    //Lieferung der Daten des Objektes und senden dieser Daten an eine SNS Topic
+    s3.getObject(params, function (err, data) {
+        if (!err) {
+            var incdata = data.Body;
+            var convdata = incdata.toString();
+
+            simpleparser(convdata,function(err,data){
+                if(err) console.log(err);
+                else var tst = [];
+                tst.push(data.from.text,data.to.text,data.subject,data.text);
+                for(var i = 0; i < tst.length; ++i){
+                    console.log(tst[i]);
+                }
+            });
 
 
-var tst = simpleparser(daten);
-
-var test = tst.then(function (value) { console.log(value); return value; });
-// console.log(test.attachments.text);
-
+        }
+        else {
+            console.log(err);
+        }
+    })
+};
 
